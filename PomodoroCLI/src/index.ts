0b6b08e63efa99bcdf1d8084afbda1c363cd14a0
Miguel dos Santos Miranda <migueldossm@gmail.com>
2025-12-13 00:25:12 -0300

    Rename CLI to pomots and add animated welcome

--- PomodoroCLI/src/index.ts
+++ PomodoroCLI/src/index.ts
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 
 import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
 
 console.clear();
 
-console.log(chalk.green("🍅 Pomodoro CLI "));
+const sleep = (ms: number = 2000): Promise<void> =>
+  new Promise(resolve => setTimeout(resolve, ms));
+
+async function welcome(): Promise<void> {
+  const title = "🍅 Pomodoro CLI\n";
+
+  const rainbowTitle = chalkAnimation.rainbow(title);
+
+  await sleep();
+  rainbowTitle.stop();
+
+  console.clear();
+
+  console.log(chalk.green(title));
+}
+
+welcome();
